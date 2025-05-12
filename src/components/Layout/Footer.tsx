16713a77ab57055c9ecb11ac79a719aefe5fede5
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-05-12 22:53:57 +0300

    feat: Update footer text to reflect the correct service name for clarity

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer className="bg-[#3d69bbda] text-white py-3">
       <div className="container mx-auto text-center">
-        <p className="text-sm">&copy; 2025 Tüm hakları saklıdır. Restoran Hizmetleri</p>
-        <p className="text-sm">Restoran Hizmetleri Ekibi tarafından tasarlandı</p>
+        <p className="text-sm">&copy; 2025 Tüm hakları saklıdır.  YEMEK BAĞIŞ HİZMETLERİ</p>
+        <p className="text-sm"> YEMEK BAĞIŞ HİZMETLERİ Ekibi tarafından tasarlandı</p>
         <p className="text-sm">Deka, Helin ve Malıka tarafından geliştirildi</p>
       </div>
     </footer>
